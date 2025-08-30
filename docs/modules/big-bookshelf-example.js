addde8e593fbd4d1e38dff40f86d52061d65f71b
Alan Smith <commit@alanwsmith.com>
2025-08-30 02:02:49 -0400

    Working on bookshelf example

--- docs/modules/big-bookshelf-example.js
+++ docs/modules/big-bookshelf-example.js
@@ -1,35 +1,24 @@
 // deno-fmt-ignore-file
 
-const viewBookDisplayTemplate = document.createElement("template");
-viewBookDisplayTemplate.innerHTML = `
-<div class="book-title"></div>
-<div>by 
-<span class="author-first-name"></span>
-<span class="author-last-name"></span>
-</div> 
-<div><span class="page-count"></span> pages</div>
-`;
+const rawTemplates = {
+    viewBookControls: `<button class="view-books" data-send="viewBooks">View All Books</button>`,
 
-const viewBooksDisplayTemplate = document.createElement("template");
-viewBooksDisplayTemplate.innerHTML = `
-<div>Book List</div>
-<div id="book-list"></div>
-`;
+    viewBookDisplay: `<div class="book-title"></div>
+<div>by <span class="author-first-name"></span>
+<span class="author-last-name"></span></div> 
+<div><span class="page-count"></span> pages</div>`,
 
-const viewBooksControlsTemplate = document.createElement("template");
-viewBooksControlsTemplate.innerHTML = `
-<button class="add-book">Add Book</button>
-`;
-
-const viewBooksBookTemplate = document.createElement("template");
-viewBooksBookTemplate.innerHTML = `
-<div>
-<span class="book-title"></span> - 
+    viewBooksBook: `<div><span class="book-title"></span> - 
 <button class="view-book" data-send="viewBook">view</button>
 <button class="edit-book">edit</button>
-<button class="delete-book">delete</button>
-</div>
-`;
+<button class="delete-book">delete</button></div>`,
+
+    viewBooksControls: `<button class="add-book">Add Book</button>`,
+
+    viewBooksDisplay: `<div>Book List</div>
+<div id="book-list"></div>`
+}
+
 
 export default class {
 
@@ -37,21 +26,37 @@ export default class {
         "25d7c22f": { title: "On Writing", author: ["Stephen", "King"], pages: 288 },
         "4bcc8077": { title: "Neuromancer", author: ["William", "Gibson"], pages: 271 },
         "fe4ae65f": { title: "Dune", author: ["Frank", "Herbert"], pages: 412 },
+        "ab9d8b45": { title: "The Diamond Age", author: ["Neal", "Stephenson"], pages: 455 },
     };
 
-    makeBookEl(bookId, bookData) {
-        const bookEl = viewBooksBookTemplate.content.cloneNode(true);
-        bookEl.querySelector(".book-title").innerHTML = bookData.title;
-        bookEl.querySelector(".view-book").dataset.id = bookId;
-        return bookEl;
+    constructor() {
+        this.templates = {};
+        for (const templateId in rawTemplates) {
+            const newTemplate = document.createElement("template");
+            newTemplate.innerHTML = rawTemplates[templateId];
+            this.templates[templateId] = newTemplate
+        } 
+        console.log(this.templates);
     }
 
-    showViewBookControls(_el, _event) {
-        
+    loadTemplate(templateId) {
+        return this.templates[templateId].content.cloneNode(true);
+    }
+
+    makeBookEl(bookId, bookData) {
+        const wrapper = this.loadTemplate("viewBooksBook");
+        wrapper.querySelector(".book-title").innerHTML = bookData.title;
+        wrapper.querySelector(".view-book").dataset.id = bookId;
+        return wrapper;
+    }
+
+    showViewBookControls(el, _event) {
+        const wrapper = this.loadTemplate("viewBookControls");
+        el.replaceChildren(wrapper);
     }
 
     showViewBookDisplay(el, event) {
-        const wrapper = viewBookDisplayTemplate.content.cloneNode(true);
+        const wrapper = this.loadTemplate("viewBookDisplay");
         const bookId = event.target.dataset.id;
         wrapper.querySelector(".book-title").innerHTML = this.#books[bookId].title;
         wrapper.querySelector(".author-first-name").innerHTML = this.#books[bookId].author[0];
@@ -60,13 +65,13 @@ export default class {
         el.replaceChildren(wrapper);
     }
 
-    showViewBooksControls(el) {
-        const wrapper = viewBooksControlsTemplate.content.cloneNode(true);
+    showViewBooksControls(el, _event) {
+        const wrapper = this.loadTemplate("viewBooksControls");
         el.replaceChildren(wrapper);
     }
 
-    showViewBooksDisplay(el) {
-        const wrapper = viewBooksDisplayTemplate.content.cloneNode(true);
+    showViewBooksDisplay(el, _event) {
+        const wrapper = this.loadTemplate("viewBooksDisplay");
         const list = wrapper.querySelector("#book-list");
         for (const bookId in this.#books) {
             list.appendChild(this.makeBookEl(bookId, this.#books[bookId]));
@@ -83,12 +88,12 @@ export default class {
         }
     }
 
-    viewBooks(el, _event) {
+    viewBooks(el, event) {
         if (el.dataset.name === "controls") {
-            this.showViewBooksControls(el);
+            this.showViewBooksControls(el, event);
         }
         if (el.dataset.name === "display") {
-            this.showViewBooksDisplay(el);
+            this.showViewBooksDisplay(el, event);
         }
     }
 }

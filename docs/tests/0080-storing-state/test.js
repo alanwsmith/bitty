780a86e1a12e672be2a0224f6c61611242ed4a7a
Alan Smith <commit@alanwsmith.com>
2025-09-05 19:21:41 -0400

    adding/updating tests

---
+++ docs/tests/0080-storing-state/test.js
@@ -0,0 +1,14 @@
+export default class {
+  #count = 0;
+  init() {
+    this.api.querySelector("button").click();
+    this.api.querySelector("button").click();
+    this.api.querySelector("button").click();
+  }
+  runTest(el, event) {
+    this.#count += 1;
+    if (this.#count === 3) {
+      el.innerHTML = "PASSED";
+    }
+  }
+}

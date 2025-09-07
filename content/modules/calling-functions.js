572269a1605da1de074bb3d73d5da6e66c36eb58
Alan Smith <commit@alanwsmith.com>
2025-09-07 10:30:45 -0400

    Cleaning up examples

--- content/modules/calling-functions.js
+++ content/modules/calling-functions.js
@@ -1,5 +1,6 @@
 export default class {
   update(_el, _event) {
-    this.api.querySelector("div").innerHTML = "Updated without data-receive attribute";
+    this.api.querySelector("div").innerHTML =
+      `Updated without data-receive attribute: ${Date.now()}`;
   }
 }

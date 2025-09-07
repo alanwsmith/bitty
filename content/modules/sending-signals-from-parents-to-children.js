5af13dd3650a5ff55710c950a2a2c00223754d9b
Alan Smith <commit@alanwsmith.com>
2025-09-07 09:38:20 -0400

    Added test passing signal with same name down to child

---
+++ content/modules/sending-signals-from-parents-to-children.js
@@ -0,0 +1,11 @@
+export default class {
+  #value = 0;
+
+  increment(el, _event) {
+    this.#value += 1;
+  }
+
+  update(el, event) {
+    el.innerHTML = `${event.target.dataset.name} is now: ${this.#value}`;
+  }
+}

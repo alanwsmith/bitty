698e0911116b28b208f833922099c94e9219e6f0
Alan Smith <commit@alanwsmith.com>
2025-09-23 13:17:15 -0400

    Added test for multiple signal forward in single this.api.forward call

--- content/tests/0230-forward-multiple-signals/test.js
+++ content/tests/0230-forward-multiple-signals/test.js
@@ -1,2 +1,23 @@
 export default class {
+  bittyInit() {
+    const button = this.api.querySelector("button");
+    button.click();
+  }
+
+  runTest(event, element) {
+    element.innerHTML = "PASSED";
+    this.api.forward(event, "secondSignal|thirdSignal|fourthSignal");
+  }
+
+  secondSignal(event, element) {
+    element.innerHTML = "PASSED";
+  }
+
+  thirdSignal(event, element) {
+    element.innerHTML = "PASSED";
+  }
+
+  fourthSignal(event, element) {
+    element.innerHTML = "PASSED";
+  }
 }

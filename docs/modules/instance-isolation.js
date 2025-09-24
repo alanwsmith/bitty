7604f2c386e57583d1629ef21e560fc9fa76a7b3
Alan Smith <commit@alanwsmith.com>
2025-09-23 21:25:43 -0400

    Updated notes

--- docs/modules/instance-isolation.js
+++ docs/modules/instance-isolation.js
@@ -2,6 +2,13 @@ export default class {
   #value = 0;
 
   updateIsolatedInstance(event, element) {
+    // The same signal name is used in both
+    // elements in this example. Separating
+    // the data can be done by comparing
+    // the bitty-js component they are
+    // connect to with the data from
+    // the triggering event.
+
     // get the uuid of the parent bitty-js
     // component
     const bittyUUID = this.api.dataset.uuid;

c5af9b98b28be2d56c55840cf4c25cc65082070b
Alan Smith <commit@alanwsmith.com>
2025-09-25 17:41:57 -0400

    Moved async test to just having one class

--- docs/tests/0250-async-bitty-init/test.js
+++ docs/tests/0250-async-bitty-init/test.js
@@ -2,16 +2,9 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export class AsyncTest {
+export default class {
   async bittyInit() {
     await sleep(100);
-    const el = this.api.querySelector("div");
-    el.innerHTML = "PASSED";
-  }
-}
-
-export class SyncTest {
-  bittyInit() {
     const el = this.api.querySelector("div");
     el.innerHTML = "PASSED";
   }

df26d4da424894f5a8a0363bba430634e5f62b99
Alan Smith <commit@alanwsmith.com>
2025-09-30 16:50:21 -0400

    prep for 1.0.0-rc5 dev

--- .build-files/modules/nested-parent-ignore.js
+++ .build-files/modules/nested-parent-ignore.js
@@ -2,18 +2,18 @@ export default class {
   template() {
     return `
 <div>
-  <bitty-v1-rc4
+  <bitty-v1-rc5
     data-connect="./nested-child.js"
     data-send="showValue"
     data-name="Counter 1"
-  ></bitty-v1-rc4>
+  ></bitty-v1-rc5>
 </div>
 <div>
-  <bitty-v1-rc4
+  <bitty-v1-rc5
     data-connect="./nested-child.js"
     data-send="showValue"
     data-name="Counter 2"
-  ></bitty-v1-rc4>
+  ></bitty-v1-rc5>
 </div>
 `;
   }
--- docs/modules/instance-isolation.js
+++ docs/modules/instance-isolation.js
@@ -5,11 +5,11 @@ export default class {
     // The same signal name is used in both
     // elements in this example. Separating
     // the data can be done by comparing
-    // the bitty-v1-rc4 component they are
+    // the bitty-v1-rc5 component they are
     // connect to with the data from
     // the triggering event.
 
-    // get the uuid of the parent bitty-v1-rc4
+    // get the uuid of the parent bitty-v1-rc5
     // component
     const bittyUUID = this.api.dataset.uuid;
 

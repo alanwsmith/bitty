2249ec5d38c47957483547ea49bcacb2d4367b96
Alan Smith <commit@alanwsmith.com>
2025-10-14 10:00:08 -0400

    cleaning up tests

--- docs/v2.0.0/tests/0230-forward-multiple-signals/test.js
+++ docs/v2.0.0/tests/0230-forward-multiple-signals/test.js
@@ -1,14 +1,4 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default class {
-  async bittyInit() {
-    await sleep(100); // time pad for test
-    const button = this.api.querySelector("button");
-    button.click();
-  }
-
   runTest0230(event, element) {
     element.innerHTML = "PASSED";
     this.api.forward(

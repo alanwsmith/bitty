8275b9c7029a9798aba8c17d6873a011cc27ac67
Alan Smith <commit@alanwsmith.com>
2025-10-13 20:49:15 -0400

    cleaning up tets

--- content/v2.0.0/tests/0010-basic-send-receive/test.js
+++ content/v2.0.0/tests/0010-basic-send-receive/test.js
@@ -1,11 +1,6 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default class {
-  async bittyInit() {
-    await sleep(100) // time pad for test
-    this.api.querySelector("button").click();
+  initTest0010(_event, el) {
+    el.click();
   }
 
   runTest0010(_event, el) {
--- content/v2.0.0/tests/0015-nestined-send-and-receive/test.js
+++ content/v2.0.0/tests/0015-nestined-send-and-receive/test.js
@@ -1,11 +1,6 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default class {
-  async bittyInit() {
-    await sleep(100) // time pad for test
-    this.api.querySelector("button").click();
+  initTest0015(_event, el) {
+    el.click();
   }
 
   runTest0015(_event, el) {

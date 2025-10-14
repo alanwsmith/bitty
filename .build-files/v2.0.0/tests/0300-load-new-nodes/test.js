c8b0336c52904ca5f5b1c204b3b5c201bf5905f5
Alan Smith <commit@alanwsmith.com>
2025-10-14 09:56:45 -0400

    cleaning up tests

--- .build-files/v2.0.0/tests/0300-load-new-nodes/test.js
+++ .build-files/v2.0.0/tests/0300-load-new-nodes/test.js
@@ -11,7 +11,7 @@ export default class {
     </div>`;
     this.api.appendChild(newTree);
     await sleep(100);
-    this.api.querySelector("button").click();
+    this.api.forward(null, "runTest0300");
   }
 
   runTest0300(_event, el) {
--- docs/v2.0.0/tests/0270-use-document-query-selector/test.js
+++ docs/v2.0.0/tests/0270-use-document-query-selector/test.js
@@ -1,8 +1,4 @@
 export default class {
-  bittyInit() {
-    this.api.querySelector("button").click();
-  }
-
   runTest0270(_event, _element) {
     const el = document.querySelector("#testDocumentQuerySelectorTarget");
     el.innerHTML = "PASSED";

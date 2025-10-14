2dd3b4c5ed646160f46d8d2759842990419ba1b1
Alan Smith <commit@alanwsmith.com>
2025-10-14 10:36:27 -0400

    cleaned up tests

--- .build-files/v2.0.0/tests/0350-change-event-listeners/test.js
+++ .build-files/v2.0.0/tests/0350-change-event-listeners/test.js
@@ -1,6 +1,13 @@
 export default class {
+  async bittyInit() {
+    this.api.addEventListener("altevent", () => {
+      this.api.forward(null, "runTest0350");
+    });
+    const event = new Event("altevent");
+    this.api.dispatchEvent(event);
+  }
+
   runTest0350(_event, element) {
     element.innerHTML = "PASSED";
-    element.classList.add("test-passed");
   }
 }
--- docs/v2.0.0/modules/test-results.js
+++ docs/v2.0.0/modules/test-results.js
@@ -2,12 +2,31 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+const t = {
+  "passed": `
+<div class="test-passed">Passed: PASSED_NUM</div>
+<div>Failed: 0</div>
+`,
+  "failed": `
+<div>Passed: PASSED_NUM</div>
+<div class="test-failed">Failed: FAILED_NUM</div>
+<p>
+  NOTE: Each test loads its own external 'test.js' file. 
+  They are checked after a brief 'setTimeout()'. 
+  If you're on a slow network connection they may
+  not all load before the check function fires. 
+  That's the first thing to check if you 
+  see failures. You can do so by cloning a
+  copy of the repo and running it locally.
+</p>
+`,
+};
+
 export default class {
   #passed = 0;
   #failed = 0;
 
   async showResults(_event, element) {
-    element.innerHTML = "Gathering test results...";
     await sleep(1200);
     const els = document.querySelectorAll(".test");
     [...els].forEach((el) => {
@@ -15,19 +34,20 @@ export default class {
         this.#passed += 1;
         el.classList.add("test-passed");
       } else {
-        console.log(el)
+        console.log(el);
         this.#failed += 1;
         el.classList.add("test-failed");
       }
     });
     if (this.#failed == 0) {
-      element.innerHTML = `
-<div class="test-passed">PASSED: ${this.#passed}</div>
-<div>Failed: ${this.#failed}</div>`;
+      const subs = [["PASSED_NUM", this.#passed]];
+      element.replaceChildren(this.api.useTemplate(t.passed, subs));
     } else {
-      element.innerHTML = `
-<div>PASSED: ${this.#passed}</div>
-<div class="test-failed">Failed: ${this.#failed}</div>`;
+      const subs = [
+        ["PASSED_NUM", this.#passed],
+        ["FAILED_NUM", this.#failed],
+      ];
+      element.replaceChildren(this.api.useTemplate(t.failed, subs));
     }
   }
 }

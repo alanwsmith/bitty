074453baa62d59d8d6546596c6cf37591f5ef277
Alan Smith <commit@alanwsmith.com>
2025-10-20 11:04:56 -0400

    updated examples

---
+++ .build-files/v2.0.0/modules/installation-example.js
@@ -0,0 +1,5 @@
+export default class {
+  randomNum(_event, el) {
+    el.innerHTML = Math.random();
+  }
+}
--- docs/v2.0.0/modules/example-class.js
+++
@@ -1,8 +0,0 @@
-export default class {
-  #num = 0;
-
-  count(_event, el) {
-    this.#num += 1;
-    el.innerHTML = `Count: ${this.#num}`; 
-  }
-}

2d5eb6dbb81768eeed669dbef348de1d10dadddf
Alan Smith <commit@alanwsmith.com>
2025-10-21 12:33:21 -0400

    Added test for undefined on getTXT

---
+++ docs/v2.0.0/tests/0495-fetch-txt-error/test.js
@@ -0,0 +1,9 @@
+export default class {
+  async runTest0495(_event, el) {
+    const url = "/v2.0.0/tests/0495-fetch-txt-error/intentionally-missing-file.txt";
+    const response = await this.api.getTXT(url);
+    if (response === undefined) {
+      el.innerHTML = "PASSED";
+    }
+  }
+}

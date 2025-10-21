441e32ed1896d9f5ac275e1384b15eaaa7344783
Alan Smith <commit@alanwsmith.com>
2025-10-21 11:25:25 -0400

    working on getJSON docs

---
+++ docs/v2.0.0/modules/get-json/subs.js
@@ -0,0 +1,13 @@
+export default class {
+  async getSubsJSON(_event, el) {
+    const subs = [
+      ["SPEED", "slow"],
+      ["KIND", "red"]
+    ];
+    const data = await this.api.getJSON(
+      "/v2.0.0/payloads/get-json/subs.json",
+      subs
+    );
+    el.innerHTML = data.text;
+  }
+}

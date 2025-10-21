c154e213cca7f81feb471aee37cbe370ee23207c
Alan Smith <commit@alanwsmith.com>
2025-10-21 14:47:37 -0400

    finished match docs

--- content/v2.0.0/_snippets/intentional-errors.js
+++ content/v2.0.0/_snippets/intentional-errors.js
@@ -1,6 +1,6 @@
 async intentionalErrors(_event, el) {
   await new Promise(resolve => setTimeout(resolve, 2400));
   console.error(`HEADS UP: There are intentional 404 errors on this page!
-The code on the page is live. That includes the examples and tests demonstrating failing fetch calls that throw errors. Any 404s can safely be ignored.`);
+The code on the page is live. That includes the examples and tests demonstrating failing fetch calls that throw errors. Any 404s can safely be disregarded.`);
 }
 
--- docs/v2.0.0/modules/match/with-key.js
+++ docs/v2.0.0/modules/match/with-key.js
@@ -1,7 +1,10 @@
 export default class {
   matchWithKey(event, el) {
     const key = "extra";
-    const doesItMatch = this.api.match(event, el, key);
-    el.innerHTML = doesItMatch;
+    if(this.api.match(event, el, key)) {
+      el.innerHTML = `Matched: ${el.dataset.extra}`;
+    } else {
+      el.innerHTML = `Did Not Match: ${el.dataset.extra}`;
+    }
   }
 }

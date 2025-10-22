cfa2d923a82515643c13573de45d4923c16d951c
Alan Smith <commit@alanwsmith.com>
2025-10-22 13:57:24 -0400

    cleaning up tests

--- docs/v3.0.0/tests/0130-create-an-element/test.js
+++ docs/v3.0.0/tests/0130-create-an-element/test.js
@@ -1,21 +1,9 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default class {
-  async runTest0130(_event, element) {
+  async runTest0130(_event, el) {
     const newDiv = document.createElement("div");
-    const newButton = document.createElement("button");
-    newButton.classList.add("test");
-    newButton.dataset.receive = "placeholderToGenerateUUID";
-    newButton.innerHTML = "FAILED";
-    newDiv.appendChild(newButton);
-    element.replaceChildren(newDiv);
-    // sleep for test to wait for observer
-    // to update the UUID.
-    await sleep(100);
-    if (newButton.dataset.uuid !== undefined) {
-      newButton.innerHTML = "PASSED";
+    await this.api.appendChild(newDiv);
+    if (newDiv.dataset.uuid !== undefined) {
+      el.innerHTML = "PASSED";
     }
   }
 }

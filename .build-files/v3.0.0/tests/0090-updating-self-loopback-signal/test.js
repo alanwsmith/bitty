aebdb663de427d2a93bff00fae9e4e3ff2870e05
Alan Smith <commit@alanwsmith.com>
2025-10-22 13:52:05 -0400

    better test formatting

--- .build-files/v3.0.0/tests/0090-updating-self-loopback-signal/test.js
+++ .build-files/v3.0.0/tests/0090-updating-self-loopback-signal/test.js
@@ -5,7 +5,7 @@ function sleep(ms) {
 export default class {
   async bittyInit() {
     await sleep(100); // time pad for test
-    const button = this.api.querySelector("button");
+    const button = this.api.querySelector("div");
     button.click();
   }
   runTest0090(_event, el) {

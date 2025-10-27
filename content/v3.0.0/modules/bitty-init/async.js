250d71b10d9dd8a6b70f8e668a9d13c8d9a05deb
Alan Smith <commit@alanwsmith.com>
2025-10-26 22:17:14 -0400

    cleanded up docs

--- content/v3.0.0/modules/bitty-init/async.js
+++ content/v3.0.0/modules/bitty-init/async.js
@@ -3,7 +3,12 @@ export default class {
 
   async bittyInit() {
     const url = "/v3.0.0/payloads/bitty-init/basic.txt";
-    this.#message = await this.api.getTXT(url);
+    const response = await this.api.getTXT(url);
+    if (response.ok) {
+      this.#message = response.ok;
+    } else {
+      this.#message = response.error;
+    }
   }
 
   update(_event, el) {

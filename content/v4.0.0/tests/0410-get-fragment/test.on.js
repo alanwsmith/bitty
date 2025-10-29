4d3e216427bf7f25095fd1c01975348a162ad23e
Alan Smith <commit@alanwsmith.com>
2025-10-29 19:58:39 -0400

    put in the rest of the vars for the test cases

--- content/v4.0.0/tests/0410-get-fragment/test.on.js
+++ content/v4.0.0/tests/0410-get-fragment/test.on.js
@@ -1,6 +1,6 @@
 export default class {
   async runTest0410(_event, el) {
-    const url = `/[@ json.version.version_dir @]/tests/0410-get-fragment/payload/index.html`;
+    const url = `/[@ json.version.version_dir @]/tests/0410-get-fragment/payload.html`;
     const response = await this.api.getFragment(url);
     el.parentNode.replaceChildren(response.ok);
   }
--- content/v4.0.0/tests/0420-get-fragment-with-subs/test.on.js
+++ content/v4.0.0/tests/0420-get-fragment-with-subs/test.on.js
@@ -1,6 +1,6 @@
 export default class {
   async runTest0420(_event, el) {
-    const url = `/[@ json.version.version_dir @]/tests/0420-get-fragment-with-subs/payload/index.html`;
+    const url = `/[@ json.version.version_dir @]/tests/0420-get-fragment-with-subs/payload.html`;
     const subs = [["FAILED", "PASSED"]];
     const response = await this.api.getFragment(url, subs);
     el.parentNode.replaceChildren(response.ok);

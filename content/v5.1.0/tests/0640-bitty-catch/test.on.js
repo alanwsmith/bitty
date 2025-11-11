a790bdb488ed6d62154b0d239ccd38680732c7f2
Alan Smith <commit@alanwsmith.com>
2025-11-11 10:08:28 -0500

    added bittyCatch test

--- content/v5.1.0/tests/0640-bitty-catch/test.on.js
+++ content/v5.1.0/tests/0640-bitty-catch/test.on.js
@@ -1,3 +1,14 @@
 export default class {
+  bittyReady() {
+    this.api.querySelector("button").click();
+  }
+
+  bittyCatch(event) {
+    this.api.trigger("runTest0640");
+  }
+
+  runTest0640(_event, el) {
+    el.innerHTML = "PASSED";
+  }
 
 }

c7e1812bf006f5f4bf4d749b261ecb6e10111504
Alan Smith <commit@alanwsmith.com>
2025-11-24 11:49:12 -0500

    added test for bittyParent and bittyParentId

---
+++ content/v6.0.0/tests/1070-check-bitty-parent/test.on.js
@@ -0,0 +1,17 @@
+export default class {
+  bittyReady() {
+    this.api.querySelectorAll(".clickme").forEach((el) => {
+      el.click();
+    });
+  }
+  runTest1070Alfa(_event, el) {
+    if (el.bittyParentId === this.api.dataset.bittyid) {
+      el.innerHTML = "PASSED";
+    }
+  }
+  runTest1070Bravo(_event, el) {
+    if (el.bittyParent.dataset.bittyid === this.api.dataset.bittyid) {
+      el.innerHTML = "PASSED";
+    }
+  }
+}

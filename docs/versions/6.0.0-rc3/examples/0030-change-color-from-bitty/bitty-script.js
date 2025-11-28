f5930e92f907c6b87fb146649a0d407dd9e3df32
Alan Smith <commit@alanwsmith.com>
2025-11-27 21:36:34 -0500

    added font size change example

--- docs/versions/6.0.0-rc3/examples/0030-change-color-from-bitty/bitty-script.js
+++ docs/versions/6.0.0-rc3/examples/0030-change-color-from-bitty/bitty-script.js
@@ -1,31 +1,24 @@
-const params = {
-  "lightness": { min: 0, max: 1.0, step: 0.01, value: 0.7 },
-  "chroma": { min: 0, max: 0.2, step: 0.001, value: 0.05 },
-  "hue": { min: 0, max: 360, step: 1, value: 300 },
-};
+const params = ["red", "green", "blue"];
 
 const sliderTemplate = `
 <div><label>PARAM
 <input 
   type="range" 
   data-param="PARAM"
-  min="MIN"
-  max="MAX"
-  step="STEP"
+  min="0"
+  max="255"
   value="VALUE"
   data-send="updateColor" />
 </label></div>`;
 
 export default class {
   makeSliders(_, el) {
-    Object.entries(params).forEach(([param, values]) => {
-      this.api.setProp(`--${param}`, values.value);
+    params.forEach((param) => {
+      const value = 200;
+      this.api.setProp(`--${param}`, value);
       const subs = [
         ["PARAM", param],
-        ["VALUE", values.value],
-        ["MIN", values.min],
-        ["MAX", values.max],
-        ["STEP", values.step],
+        ["VALUE", value],
       ];
       const slider = this.api.makeHTML(sliderTemplate, subs);
       el.appendChild(slider);

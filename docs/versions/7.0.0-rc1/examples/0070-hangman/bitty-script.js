1ce1cf2db3593179abb6f7898645824a566b95af
Alan Smith <commit@alanwsmith.com>
2025-11-28 20:27:42 -0500

    working on hangman

--- docs/versions/7.0.0-rc1/examples/0070-hangman/bitty-script.js
+++ docs/versions/7.0.0-rc1/examples/0070-hangman/bitty-script.js
@@ -1,17 +1,94 @@
-const words = ["football", "running", "quiet"];
-export default class {
-  #currentWord = 0;
-  #guesses = [];
+const startTemplate = `
+<button data-send="hangmanStartGame">Start Game</button> 
+`;
+const inputTemplate = `
+<label>Guess:
+<input data-send="hangmanLetter" type="text" size=3" />
+</label>`;
 
-  bittyReady() {
-    this.api.trigger("updateGallows hangmanDisplay");
+const gameOverTemplate = `
+<div>Game Over - <button data-send="hangmanRestart">Restart</button></div>
+`;
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export default class {
+  #letters = "football".split("");
+  #matches = new Set();
+  #guesses = new Set();
+  #playing = "starting";
+
+  bittyInit() {
+    //this.api.trigger("hangmanInterface");
   }
 
-  updateGallows(_ev, el) {
-    el.innerHTML = 0;
+  bittyReady() {
+    //this.api.trigger("hangmanGallows hangmanDisplay");
+  }
+
+  hangmanStartButton(_ev, el) {
+    el.replaceChildren(this.api.makeHTML(startTemplate));
+  }
+
+  hangmanStartGame(_ev, el) {
+    this.#playing = "playing";
+    this.api.trigger("hangmanGallows hangmanDisplay hangmanInterface");
+  }
+
+  hangmanInterface(_ev, el) {
+    if (this.#playing === "playing") {
+      el.replaceChildren(this.api.makeHTML(inputTemplate));
+    } else if (this.#playing === "over") {
+      el.replaceChildren(this.api.makeHTML(gameOverTemplate));
+    }
+  }
+
+  hangmanRestart(_ev, el) {
+    this.#playing = "playing";
+    this.#guesses = new Set();
+    this.api.trigger(`
+      hangmanInterface
+      hangmanGallows
+      hangmanDisplay`);
+  }
+
+  hangmanGallows(_ev, el) {
+    let misses = 0;
+    this.#guesses.forEach((guess) => {
+      if (!this.#letters.includes(guess)) {
+        misses += 1;
+      } else {
+        this.#matches.add(guess);
+      }
+    });
+    el.innerHTML = misses;
+    if (misses === 5) {
+      this.#playing = "over";
+      this.api.trigger("hangmanInterface");
+    }
   }
 
   hangmanDisplay(_ev, el) {
-    el.innerHTML = "_ _ _ _ _ _ _ _";
+    const output = this.#letters.map((letter) => {
+      if (this.#guesses.has(letter)) {
+        return letter;
+      } else {
+        return "_";
+      }
+    });
+    el.innerHTML = output.join(" ");
+  }
+
+  async hangmanLetter(ev, _el) {
+    if (ev.stringValue !== "") {
+      ev.target.readOnly = true;
+      this.#guesses.add(ev.stringValue);
+      this.api.trigger("hangmanDisplay hangmanGallows");
+      await sleep(100);
+      ev.target.value = "";
+      ev.target.readOnly = false;
+    }
   }
 }

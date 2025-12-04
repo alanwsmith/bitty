797e21d24fab25a14bb2753e0c1ba87a7be0a514
Alan Smith <commit@alanwsmith.com>
2025-12-04 16:58:48 -0500

    started move to major vesrion dir structure

--- docs/versions/6.0.0-rc3/tests/0420-get-html-with-subs/test.on.js
+++
@@ -1,8 +0,0 @@
-export default class {
-  async runTest0420(_event, el) {
-    const url = "/versions/6.0.0-rc3/tests/0420-get-html-with-subs/payload.html";
-    const subs = [["FAILED", "PASSED"]];
-    const response = await this.api.getHTML(url, subs);
-    el.parentNode.replaceChildren(response.value);
-  }
-}
---
+++ docs/versions/7/0/0-rc1/tests/0394-get-json-parse-error/test.on.js
@@ -0,0 +1,13 @@
+export default class {
+  bittyInit() {
+    this.api.trigger("runTest0394");
+  }
+
+  async runTest0394(_event, el) {
+    const url = "/versions/7/0/0-rc1/tests/0394-get-json-parse-error/invalid-json.json";
+    const response = await this.api.getJSON(url);
+    if (response.error.type === "parsing") {
+      el.innerHTML = "PASSED";
+    }
+  }
+}

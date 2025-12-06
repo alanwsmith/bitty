c8ac4833d6a5f3f99cdb86cfb81538f397fed7aa
Alan Smith <commit@alanwsmith.com>
2025-12-06 12:32:11 -0500

    working on dos

--- content/versions/7/0/0-rc2/documentation/_includes/helper-properties/el.valueToInt/_examples/0010-basic-usage/javascript.js
+++ content/versions/7/0/0-rc2/documentation/_includes/helper-properties/el.valueToInt/_examples/0010-basic-usage/javascript.js
@@ -1,3 +1,12 @@
 window.[@ item.name|title @][@ item.memberof|title @] = class {
+  #incomingValue = null;
 
+  valueToIntElExample(_, el) {
+    this.#incomingValue = el.valueToInt;
+    this.api.localTrigger("valueToIntElExampleOutput");
+  }
+
+  valueToIntElExampleOutput(_, el) {
+    el.innerHTML = this.#incomingValue;
+  }
 }

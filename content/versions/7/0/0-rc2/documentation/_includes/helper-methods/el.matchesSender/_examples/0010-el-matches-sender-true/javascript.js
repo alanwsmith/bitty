780ac28bbe58eb1396aa7e1308b7ea1747cd61ad
Alan Smith <commit@alanwsmith.com>
2025-12-06 18:23:01 -0500

    working on docs

---
+++ content/versions/7/0/0-rc2/documentation/_includes/helper-methods/el.matchesSender/_examples/0010-el-matches-sender-true/javascript.js
@@ -0,0 +1,9 @@
+window.[@ item.name|title @][@ item.memberof|title @] = class {
+  bittyReady() {
+    this.api.querySelector("button").click();
+  }
+  elMatchesSender(_, el) {
+    el.innerHTML = el.matchesSender("item");
+  }
+}
+

59b24c63b192dedd44526cff3557771ac819739d
Alan Smith <commit@alanwsmith.com>
2025-12-06 14:15:45 -0500

    working on docs

--- content/versions/7/0/0-rc2/documentation/_includes/helper-properties/ev.sender/_examples/0010-basic-usage/javascript.js
+++ content/versions/7/0/0-rc2/documentation/_includes/helper-properties/ev.sender/_examples/0010-basic-usage/javascript.js
@@ -1,3 +1,8 @@
 window.[@ item.name|title @][@ item.memberof|title @] = class {
-
+  bittyReady() {
+    this.api.querySelector("button").click();
+  }
+  senderEvSender(ev, el) {
+    el.innerHTML = ev.sender.dataset.bittyid;
+  }
 }

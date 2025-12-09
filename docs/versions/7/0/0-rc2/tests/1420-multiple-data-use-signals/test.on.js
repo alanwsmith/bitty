d68219c71849cf231104c20884f5171acc05a913
Alan Smith <commit@alanwsmith.com>
2025-12-09 16:01:53 -0500

    added test for data-use with multiple signals

---
+++ docs/versions/7/0/0-rc2/tests/1420-multiple-data-use-signals/test.on.js
@@ -0,0 +1,14 @@
+export default class {
+  bittyReady() {
+    this.api.querySelector(".clickme1420").click();
+  }
+
+  runTest1420Alfa(ev, el) {
+    el.innerHTML = "WORKING";
+  }
+
+  runTest1420Bravo(ev, el) {
+    el.innerHTML = "PASSED";
+  }
+
+}

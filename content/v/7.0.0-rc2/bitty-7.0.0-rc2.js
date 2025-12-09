89642d9e90352a303c02857afb278ad512d62122
Alan Smith <commit@alanwsmith.com>
2025-12-09 15:56:44 -0500

    cleanup

--- content/v/7.0.0-rc2/bitty-7.0.0-rc2.js
+++ content/v/7.0.0-rc2/bitty-7.0.0-rc2.js
@@ -446,52 +446,6 @@ class BittyJs extends HTMLElement {
         //     }
         //   }
         // }
-        // if (ev.sender.dataset.send) {
-        //   // Process data-receive elements
-        //   const signals = this.trimInput(ev.sender.dataset.send);
-        //   const receivers = this.querySelectorAll("[data-receive]");
-        //   for (let signal of signals) {
-        //     let doAwait = false;
-        //     const iSigParts = signal.split(":");
-        //     if (iSigParts.length === 2 && iSigParts[0] === "await") {
-        //       doAwait = true;
-        //       signal = iSigParts[1];
-        //     }
-        //     if (this.conn[signal]) {
-        //       let foundReceiver = false;
-        //       for (let receiver of receivers) {
-        //         // TODO: Remove this
-        //         // receiver.sender = sender;
-        //         const receptors = this.trimInput(receiver.dataset.receive);
-        //         for (const receptor of receptors) {
-        //           const rSignalParts = receptor.split(":");
-        //           if (
-        //             rSignalParts.length === 2 && rSignalParts[0] === "await"
-        //           ) {
-        //             receptor = rSignalParts[1];
-        //             doAwait == true;
-        //           }
-        //           if (receptor === signal) {
-        //             foundReceiver = true;
-        //             this.expandElement(ev, receiver);
-        //             if (doAwait) {
-        //               await this.conn[signal](ev, receiver);
-        //             } else {
-        //               this.conn[signal](ev, receiver);
-        //             }
-        //           }
-        //         }
-        //       }
-        //       if (foundReceiver === false) {
-        //         if (doAwait) {
-        //           await this.conn[signal](ev, null);
-        //         } else {
-        //           this.conn[signal](ev, null);
-        //         }
-        //       }
-        //     }
-        //   }
-        // }
 
       }
     }

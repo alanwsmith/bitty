3da974e78668fc6769d11aeff3776fc8143bb7e5
Alan Smith <commit@alanwsmith.com>
2025-12-10 15:01:29 -0500

    migrating docs

---
+++ content/documentation/7/0/0/_includes/attrs/data-receive/receive-multiple/script.js
@@ -0,0 +1,11 @@
+window.ReceiveMultiple = class {
+  receiveAlfa(_, el) {
+    el.innerHTML = "Got Alfa";
+  }
+  receiveBravo(_, el) {
+    el.innerHTML = "Got Bravo";
+  }
+  receiveCharlie(_, el) {
+    el.innerHTML = "Got Charlie";
+  }
+}

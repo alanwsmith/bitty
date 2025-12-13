2e96333123ee91d76b0d63fc46eedf04892fcda2
Alan Smith <commit@alanwsmith.com>
2025-12-12 20:40:26 -0500

    working on doc test runner

--- docs/projects/7/0/0/docs-test-runner/script.js
+++ docs/projects/7/0/0/docs-test-runner/script.js
@@ -5,9 +5,21 @@ function sleep(ms) {
 window.BittyDocTestRunner = class {
   async bittyReady() {
     await sleep(300);
-    // this.api.localTrigger("restTesults");
+    this.api.localTrigger("testResults");
   }
 
   testResults(_, el) {
+    let totalTest = 0;
+    let failed = 0;
+    document.querySelectorAll("[data-expected]").forEach((el) => {
+      totalTest += 1;
+      if (el.dataset.expected === el.innerHTML.trim()) {
+        el.classList.add("doc-test-passed");
+      } else {
+        failed += 1;
+        el.classList.add("doc-test-failed");
+      }
+    });
+    el.innerHTML = "test results";
   }
 };

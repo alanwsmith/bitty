83bc156294fb5aafcf73c4288d0ab02a44ccad58
Alan Smith <commit@alanwsmith.com>
2025-12-13 21:53:12 -0500

    working on docs

--- content/documentation/7/0/0/_includes/el.methods/el.propToFloat/_examples/0050-nan-if-not-a-number/javascript.js
+++ content/documentation/7/0/0/_includes/el.methods/el.propToFloat/_examples/0050-nan-if-not-a-number/javascript.js
@@ -1,5 +1,6 @@
 window.[@ item.name|title @][@ item.memberof|title @][@ supplemental_string @] = class {
   [@ method_name @](_, el) {
-    el.innerHTML = "TODO";
+    el.innerHTML = el.propToFloat("nokeywiththisname")
   }
 }
+

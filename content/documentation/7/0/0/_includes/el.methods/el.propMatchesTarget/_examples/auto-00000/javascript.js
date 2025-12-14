ea8851f4ee74a2abdb0b577b6d6239da9dbc5379
Alan Smith <commit@alanwsmith.com>
2025-12-14 10:10:17 -0500

    working on docs

---
+++ content/documentation/7/0/0/_includes/el.methods/el.propMatchesTarget/_examples/auto-00000/javascript.js
@@ -0,0 +1,11 @@
+[# #########################################
+
+DO NOTE EDIT THIS FILE MANUALLY
+Use: helpers/match_makers_without_el_key/make_tests.py
+
+######################################### #]
+
+window.[@ item.name|title @][@ item.memberof|title @][@ supplemental_string @] = class {
+}
+
+

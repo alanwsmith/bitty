3d678ad3136534bbeeb53fde51b73c2eebb7b012
Alan Smith <commit@alanwsmith.com>
2025-12-14 09:31:48 -0500

    working out output for tests

--- helpers/match_makers/match_makers.py
+++ helpers/match_makers/match_makers.py
@@ -8,37 +8,36 @@ keys = [
 		"propMatchesSender"
 		]
 
+lower_keys = [
+		"target",
+		"sender"
+		]
+
+segments = [
+		"el", "el ancestor", "ev", "ev ancestor"
+]
+
 statuses = [
-	"has data-key",
-	"does not have data-key"
+	"DOES NOT HAVE",
+	"HAS",
 ]
 
 matches = [
-	"values match",
-	"values do not match"
+	"do not match",
+	"match",
 ]
 
-def payload():
-	items = []
-	for i in range(0,32):
-		m = matches[(i >> 4) & 1]
-		if ((i >> 0) & 1 == 0 and (i >> 1) & 1 == 0):
-			continue
-		if ((i >> 0) & 1 == 1 and (i >> 1) & 1 == 1):
-			continue
-		if ((i >> 2) & 1 == 0 and (i >> 3) & 1 == 0):
-			continue
-		if ((i >> 2) & 1 == 1 and (i >> 3) & 1 == 1):
-			continue
-		items.append([
-			(i >> 0) & 1, 
-			(i >> 1) & 1, 
-			(i >> 2) & 1, 
-			(i >> 3) & 1, 
-			(i >> 4) & 1, 
-		])
-	items.sort()
-	return items
+targets = [
+	"false",
+	"true",
+]
+
+
+
+
+def item_number(item):
+	return "".join([(str(x)) for x in item])
+
 
 def report_line(item):
 	el = statuses[item[0]]
@@ -46,7 +45,7 @@ def report_line(item):
 	ev = statuses[item[2]]
 	eva = statuses[item[3]]
 	m = matches[item[4]]
-	return f"el {el} - ancestor of el {ela} - ev {ev} - ancestor of ev {eva} - {m}"
+	return f"""{segments[0]} {el} - {segments[1]} {ela} - {segments[2]} {ev} - {segments[3]} {eva} - {m}"""
 
 
 def print_report():
@@ -77,15 +76,139 @@ def output_dir(key, item):
 	return example_dir 
 
 
+def make_data_key(item, item_index, key):
+	if item[item_index] == 0:
+		return ""
+	else:
+		return f"""data-testkey="{key}{item_number(item)}" """
+
+
 def make_html_files():
 	with open("templates/html.html") as _in:
+		template = Template(_in.read())
+		for key_index in range(0,len(keys)):
+			for item in payload():
+				data = {
+						"EL_ITEM_DATA": make_data_key(item, 0, lower_keys[key_index]),
+						"EL_ANCESTOR_DATA": make_data_key(item, 1, lower_keys[key_index]),
+						"EV_ITEM_DATA": make_data_key(item, 2, lower_keys[key_index]),
+						"EV_ANCESTOR_DATA": make_data_key(item, 3, lower_keys[key_index])
+						}
+				output = template.substitute(data)
+				output_path = f"{output_dir(keys[key_index], item)}/html.html"
+				write_file(output, output_path)
+	
+def make_supplementals():
+	with open("templates/_supplemental_string.txt") as _in:
+		template = Template(_in.read())
+		for key in keys:
+			for item in payload():
+				data = {
+						"KEY": item_number(item)
+						}
+				output = template.substitute(data)
+				output_path = f"{output_dir(key, item)}/_supplemental_string.txt"
+				write_file(output, output_path)
+
+	
+def make_method_names():
+	with open("templates/_method_name.txt") as _in:
+		template = Template(_in.read())
+		for key in keys:
+			for item in payload():
+				data = {
+					"NAME": f"el{key}{item_number(item)}"
+				}
+				output = template.substitute(data)
+				output_path = f"{output_dir(key, item)}/_method_name.txt"
+				write_file(output, output_path)
+	
+def make_target_values():
+	with open("templates/_target_value.txt") as _in:
+		template = Template(_in.read())
+		for key in keys:
+			for item in payload():
+				target = "true"
+				if item[4] == 1:
+					target = "false"
+				data = {
+						"TARGET": target
+						}
+				output = template.substitute(data)
+				output_path = f"{output_dir(key, item)}/_target_value.txt"
+				write_file(output, output_path)
+
+def name_string(key, line):
+	return f"{key} {line} data-key"
+
+def make_names():
+	with open("templates/name.txt") as _in:
+		template = Template(_in.read())
+		for key_index in range(0,len(keys)):
+			for item in payload():
+				data = {}
+				data["EL"] = name_string(segments[0], statuses[item[0]])
+				data["AEL"] = name_string(f"{segments[1]}.{lower_keys[key_index]}", statuses[item[1]])
+				data["EV"] = name_string(segments[2], statuses[item[2]])
+				data["AEV"] = name_string(f"{segments[3]}.{lower_keys[key_index]}", statuses[item[3]])
+				data["MATCHES"] = matches[item[4]]
+				data["TARGET"] = targets[item[4]]
+				output = template.substitute(data)
+				output_path = f"{output_dir(keys[key_index], item)}/name.txt"
+				write_file(output, output_path)
+	
+
+def make_descriptions():
+	with open("templates/description.html") as _in:
 		template = Template(_in.read())
 		for key in keys:
 			for item in payload():
 				data = {}
 				output = template.substitute(data)
-				output_path = f"{output_dir(key, item)}/html.html"
+				output_path = f"{output_dir(key, item)}/description.html"
 				write_file(output, output_path)
+	
+
+def make_javascript():
+	with open("templates/javascript.js") as _in:
+		template = Template(_in.read())
+		for key in keys:
+			for item in payload():
+				data = {}
+				output = template.substitute(data)
+				output_path = f"{output_dir(key, item)}/javascript.js"
+				write_file(output, output_path)
+	
+
+def make_postscripts():
+	with open("templates/postscript.html") as _in:
+		template = Template(_in.read())
+		for key in keys:
+			for item in payload():
+				data = {}
+				output = template.substitute(data)
+				output_path = f"{output_dir(key, item)}/postscript.html"
+				write_file(output, output_path)
+	
+
+def payload():
+	items = []
+	for i in range(0,32):
+		# if ((i >> 0) & 1 == 0 and (i >> 1) & 1 == 0):
+		# 	continue
+		items.append([
+			(i >> 0) & 1, 
+			(i >> 1) & 1, 
+			(i >> 2) & 1, 
+			(i >> 3) & 1, 
+			(i >> 4) & 1, 
+		])
+	print(len(items))
+	items.sort()
+	items.reverse()
+	return items
+
+
 
 
 def write_file(data, path):
@@ -94,6 +217,13 @@ def write_file(data, path):
 
 	
 if __name__ == "__main__":
-	make_html_files()
+	# make_html_files()
+	# make_supplementals()
+	# make_method_names()
+	# make_target_values()
+	# make_names()
+	# make_javascript()
+	# make_postscripts()
+	# make_descriptions()
 	print_report()
 

30c220e055dcdc82e190591f231fbbc4abef746a
Alan Smith <commit@alanwsmith.com>
2025-12-29 17:44:19 -0500

    working on docs

---
+++ content/documentation/7/0/0/_includes/ev.methods/ev.propToInt/_examples/generated-0050/javascript.js
@@ -0,0 +1,17 @@
+[# #########################################
+
+DO NOTE EDIT THIS FILE MANUALLY
+Use the document test maker python script
+
+
+######################################### #]
+
+window.TestEvMethodsEvProptoint0050 = class {
+    bittyReady() {
+    this.api.querySelector("button").click();
+  }
+
+  runEvMethodsEvProptoint0050(ev, el) {
+    el.innerHTML = ev.propToInt("delta")
+  }
+}

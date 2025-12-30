6e219081a3500fc8e3d52cb0793987cf17e73cf6
Alan Smith <commit@alanwsmith.com>
2025-12-29 19:26:33 -0500

    working on docs

---
+++ content/documentation/7/0/0/_includes/ev.sender.methods/ev.sender.prop/_examples/generated-0010/javascript.js
@@ -0,0 +1,18 @@
+[# #########################################
+
+DO NOTE EDIT THIS FILE MANUALLY
+Use the document test maker python script
+
+
+######################################### #]
+
+window.TestEvSenderMethodsEvSenderProp0010 = class {
+    bittyReady() {
+    this.api.querySelector("button").click();
+  }
+
+  runEvSenderMethodsEvSenderProp0010(ev, el) {
+    el.innerHTML = ev.sender.prop("evsendermethodsevsenderprop0010")
+  }
+
+}

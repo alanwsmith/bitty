09088dab55ebb6aaf98bce8ce233e552d2dc85e0
Alan Smith <commit@alanwsmith.com>
2025-12-29 19:11:48 -0500

    working on docs

--- content/documentation/7/0/0/_includes/ev.properties_original/ev.value/_examples/0010-basic-usage/javascript.js
+++
@@ -1,8 +0,0 @@
-window.[@ item.name|title @][@ item.memberof|title @] = class {
-  bittyReady() {
-    this.api.querySelector("input").click();
-  }
-  valueEv(ev, el) {
-    el.innerHTML = ev.value;
-  }
-}

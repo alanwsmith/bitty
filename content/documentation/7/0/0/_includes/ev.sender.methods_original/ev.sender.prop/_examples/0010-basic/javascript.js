bd538e13efd72bd2d081163f774a212f2dd0e547
Alan Smith <commit@alanwsmith.com>
2025-12-29 19:29:34 -0500

    removed deprecated files

--- content/documentation/7/0/0/_includes/ev.sender.methods_original/ev.sender.prop/_examples/0010-basic/javascript.js
+++
@@ -1,8 +0,0 @@
-window.[@ item.name|title @]EvSender = class {
-  bittyReady() {
-    this.api.querySelector("button").click();
-  }
-  propEvSender(ev, el) {
-    el.innerHTML = ev.sender.prop("item");
-  }
-}

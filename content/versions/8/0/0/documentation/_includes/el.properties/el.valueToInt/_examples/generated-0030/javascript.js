9e52b742c40ab2b22cdf0725160886a4f47b5f6b
Alan Smith <commit@alanwsmith.com>
2026-02-08 18:59:00 -0500

    corrected test

--- content/versions/8/0/0/documentation/_includes/el.properties/el.valueToInt/_examples/generated-0030/javascript.js
+++ content/versions/8/0/0/documentation/_includes/el.properties/el.valueToInt/_examples/generated-0030/javascript.js
@@ -8,6 +8,10 @@ Use: versions/#/#/#/documentation/_test_maker/make_test.py
 window.TestElPropertiesElValuetoint0030 = class {
     #incomingValue = null;
 
+  bittyReady() {
+    this.api.localTrigger("[@ method_name @]");
+  }
+
   [@ method_name @](_, el) {
     this.#incomingValue = el.valueToInt;
     this.api.localTrigger("[@ method_name @]Output");

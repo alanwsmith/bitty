623a8f3671feed09b808bd54ee379a23dc05edc3
Alan Smith <commit@alanwsmith.com>
2026-02-09 16:38:35 -0500

    Added test for copyTEXT that checks from innerHTML

---
+++ content/versions/8/0/0/documentation/_includes/el.methods/el.copyTEXT/_examples/generated-0020/javascript.js
@@ -0,0 +1,17 @@
+[# #########################################
+
+DO NOTE EDIT THIS FILE MANUALLY
+Use: versions/#/#/#/documentation/_test_maker/make_test.py
+
+######################################### #]
+
+window.TestElMethodsElCopytext0020 = class {
+    bittyReady() {
+    this.api.localTrigger("runElMethodsElCopytext0020");
+  }
+
+  runElMethodsElCopytext0020(_, el) {
+    el.copyTEXT();
+  }
+
+}

b19680a66afd38149fda7d7fbf05715922929737
Alan Smith <commit@alanwsmith.com>
2026-02-10 17:01:31 -0500

    working to move in basic trigger from page class

--- content/v/8.0.0-beta2/bitty-8.0.0-beta2.dev.js
+++ content/v/8.0.0-beta2/bitty-8.0.0-beta2.dev.js
@@ -5,6 +5,51 @@ const tagName = `bitty-${version[0]}-${version[1]}`;
 class BittyJs extends HTMLElement {
   constructor() {
     super();
+    console.log("Connected to script file");
+  }
+
+  /** internal */
+  async connectedCallback() {
+    await this.makeConnection();
+    if (this.conn) {
+      this.conn.api = this;
+      // this.handleEventBridge = this.handleEvent.bind(this);
+      //this.addEventListeners();
+      // this.loadPageData();
+      // this.loadPageTemplates();
+      await this.runBittyReady();
+    }
+  }
+
+  async handleEvent(ev) {
+  }
+
+  makeConnection() {
+    // just working with call on the window for now
+    this.conn = new window.BittyClass();
+  }
+
+  /** internal */
+  async runBittyReady() {
+    if (typeof this.conn.bittyReady === "function") {
+      if (this.conn.bittyReady[Symbol.toStringTag] === "AsyncFunction") {
+        await this.conn.bittyReady();
+      } else {
+        this.conn.bittyReady();
+      }
+    }
+  }
+
+  trigger(signal) {
+    const ev = new TriggerEvent(signal);
+    this.dispatchEvent(ev);
+  }
+}
+
+class TriggerEvent extends Event {
+  constructor(signal) {
+    super("bittytrigger", { bubbles: true });
+    this.signal = signal;
   }
 }
 

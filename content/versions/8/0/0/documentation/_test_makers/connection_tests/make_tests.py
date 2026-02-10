717fd0c0d4570183ed5d0a9dac56c86104336837
Alan Smith <commit@alanwsmith.com>
2026-02-10 16:17:29 -0500

    doing initail output from tests for connection

--- content/versions/8/0/0/documentation/_test_makers/connection_tests/make_tests.py
+++ content/versions/8/0/0/documentation/_test_makers/connection_tests/make_tests.py
@@ -4,13 +4,34 @@ import os
 
 from pathlib import Path 
 
+def slurp(path):
+    if os.path.isfile(path):
+        with open(path) as _in:
+            return _in.read()
+    else:
+        return ""
+
 class Test():
     def __init__(self, dirItem):
         self.dir = dirItem[0]
 
+    def html_output_path(self):
+        return os.path.join(self.output_dir(), "html.html")
+
     def output_dir(self):
         path_parts = self.dir.split("/")
         return os.path.join("../../_includes", path_parts[1], "items", path_parts[2], "tests", path_parts[3])
+
+
+    def html_content(self):
+        path = os.path.join(self.dir, "html.html")
+        content = slurp(path)
+        return content
+
+    def write_files(self):
+        with open(self.html_output_path(), "w") as _html:
+            _html.write(self.html_content())
+
 
 
 class TestMaker():
@@ -22,7 +43,7 @@ class TestMaker():
         for test in [self.make_test(dir) for dir in os.walk("_tests")]:
             if test is not None:
                 Path(test.output_dir()).mkdir(exist_ok=True)
-
+                test.write_files()
 
 
 if __name__ == "__main__":

c2e9a0a258262b4cc825482a640bfae517975c14
Alan Smith <commit@alanwsmith.com>
2026-02-10 16:49:25 -0500

    making initial connection to bitty script

--- docs/v/8.0.0-beta2/bitty-8.0.0-beta2.dev.js
+++ docs/v/8.0.0-beta2/bitty-8.0.0-beta2.dev.js
@@ -3,6 +3,9 @@ const version = [8, 0, 0];
 const tagName = `bitty-${version[0]}-${version[1]}`;
 
 class BittyJs extends HTMLElement {
+  constructor() {
+    super();
+  }
 }
 
 customElements.define(tagName, BittyJs);

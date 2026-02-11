b3ad5f8ea4cc66a69c9e236bea47af19864e5eda
Alan Smith <commit@alanwsmith.com>
2026-02-10 22:28:36 -0500

    added bit to keep details element state between refreshes

--- docs/versions/8/0/0/bits/details-opener.js
+++ docs/versions/8/0/0/bits/details-opener.js
@@ -1,7 +1,45 @@
 export default class {
-  #storageName = "detailsWatcher";
+  #storageKey = "detailsOpener";
+  #states;
 
   bittyReady() {
-    console.log("loaded details opener");
+    this.loadStates();
+    this.addListener();
+  }
+
+  addListener() {
+    document.querySelectorAll("details")
+      .forEach((el) => {
+        el.addEventListener("toggle", (ev) => {
+          this.updateStates();
+        });
+      });
+  }
+
+  loadStates() {
+    this.#states = this.api.getStorageOr(
+      this.#storageKey,
+      { elements: [] },
+    );
+    const elements = document.querySelectorAll("details");
+    if (elements.length !== this.#states.elements.length) {
+      this.#states = { elements: [] };
+    }
+    elements.forEach((el, elIndex) => {
+      if (this.#states.elements[elIndex]) {
+        el.open = true;
+      }
+    });
+  }
+
+  updateStates() {
+    const elements = document.querySelectorAll("details");
+    if (elements.length !== this.#states.elements.length) {
+      this.#states = { elements: [] };
+    }
+    elements.forEach((el, elIndex) => {
+      this.#states.elements[elIndex] = el.open;
+    });
+    this.api.setStorage(this.#storageKey, this.#states);
   }
 }

56d4e681e7a2f7000c3f3752eb9eba4e2735cebe
Alan Smith <commit@alanwsmith.com>
2026-02-11 00:54:39 -0500

    moved test report to insividual lines

--- docs/versions/8/0/0/bits/documentation-test-reporter.js
+++ docs/versions/8/0/0/bits/documentation-test-reporter.js
@@ -1,33 +1,57 @@
 export default class {
   async bittyReady() {
-    this.api.trigger("initSectionTestStatus");
-    await this.api.sleep(1800);
-    this.api.trigger("updateSectionResults sectionTestStatus");
+    //    this.api.trigger("initSectionTestStatus");
+    await this.api.sleep(100);
+    this.api.trigger("updateIndividualTest individualTestStatus");
+
+    //   this.api.trigger("updateSectionResults sectionTestStatus");
   }
 
-  initSectionTestStatus(_, el) {
-    el.innerHTML = "calculating test results";
+  individualTestStatus(_, el) {
+    console.log(el);
+    const parent = el.closest(".individual-test");
+    if (parent.dataset.failed !== "0") {
+      el.dataset.testStatus = "failed";
+      el.innerHTML = `failed`;
+    } else {
+      el.dataset.testStatus = "passed";
+      el.innerHTML = `passed`;
+    }
   }
 
-  sectionTestStatus(_, el) {
-    el.replaceChildren("TODO: Test Status");
-  }
+  // sectionTestStatus(_, el) {
+  //   // TODO: Update this to use el.propAsInt("passed")
+  //   // when .propAsInt() is done.
+  //   const parent = el.closest(".documentation-section");
+  //   if (parent.dataset.failed !== "0") {
+  //     el.dataset.testStatus = "failed";
+  //     el.innerHTML = `failed`;
+  //   } else {
+  //     el.dataset.testStatus = "passed";
+  //     el.innerHTML = `passed`;
+  //   }
+  // }
 
-  updateSectionResults(_, el) {
+  updateIndividualTest(_, el) {
     const passed = [...el.querySelectorAll(".test")].filter((testEl) => {
       return testEl.innerHTML.trim() === "PASSED";
     }).map((testEl) => {
       testEl.dataset.testStatus = "passed";
       return testEl;
     }).length;
+    // TODO: Update this to use `el.setProp("passed", passed)`
+    // when `.setProp()` is ready
     el.dataset.passed = passed;
-
     const failed = [...el.querySelectorAll(".test")].filter((testEl) => {
       return testEl.innerHTML.trim() !== "PASSED";
     }).map((testEl) => {
       testEl.dataset.testStatus = "failed";
       return testEl;
     }).length;
+    // TODO: Update this to use `el.setProp("failed", failed)`
+    // when `.setProp()` is ready
     el.dataset.failed = failed;
   }
+
+  //
 }

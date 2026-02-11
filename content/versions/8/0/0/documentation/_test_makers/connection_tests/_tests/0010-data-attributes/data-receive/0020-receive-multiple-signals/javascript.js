efafd0a660a2b43a2c56b08ffdc3d006c5338ada
Alan Smith <commit@alanwsmith.com>
2026-02-11 13:37:16 -0500

    updated test for mutiple bitty recieves

--- content/versions/8/0/0/documentation/_test_makers/connection_tests/_tests/0010-data-attributes/data-receive/0020-receive-multiple-signals/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/connection_tests/_tests/0010-data-attributes/data-receive/0020-receive-multiple-signals/javascript.js
@@ -1,15 +1,25 @@
 window.$CLASS_NAME = class {
+  #updates = [];
+
   bittyReady() {
     this.api.trigger(
-      `$SIGNAL_NAME $SIGNAL2_NAME`,
+      `$SIGNAL_NAME $SIGNAL2_NAME $SIGNAL3_NAME`,
     );
   }
 
   $SIGNAL_NAME(_, el) {
-    el.innerHTML = "UPDATED";
+    this.#updates.push("bug");
+    el.innerHTML = this.#updates[0];
   }
 
   $SIGNAL2_NAME(_, el) {
-    el.innerHTML = "ok";
+    this.#updates.push("bug");
+    el.innerHTML = this.#updates[1];
+  }
+
+  $SIGNAL3_NAME(_, el) {
+    if (this.#updates.length === 2) {
+      el.innerHTML = "ok";
+    }
   }
 };

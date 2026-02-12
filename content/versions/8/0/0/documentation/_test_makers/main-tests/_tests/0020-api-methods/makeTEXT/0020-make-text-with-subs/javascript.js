c2ed5a7e487da5dcc84586859449f718b8b97cb5
Alan Smith <commit@alanwsmith.com>
2026-02-12 12:38:30 -0500

    update test for makeTEXT

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/makeTEXT/0020-make-text-with-subs/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/makeTEXT/0020-make-text-with-subs/javascript.js
@@ -4,8 +4,12 @@ window.$CLASS_NAME = class {
   }
 
   $SIGNAL_NAME(ev, el) {
-    const template = "REPLACE_ALFA REPLACE_BRAVO REPLACE_ALFA";
-    const subs = [];
-    el.innerHTML = "xxx";
+    const template = "TARGET_ALFA";
+    const subs = ["TARGET_ALFA", "updated_alfa"];
+    const expected = "updated_alfa";
+    const got = this.api.makeTEXT(template, subs);
+    if (expected === got) {
+      el.innerHTML = "ok";
+    }
   }
 };
--- content/versions/8/0/0/documentation/includes/0020-api-methods/items/makeTEXT/tests/0020-make-text-with-subs/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-api-methods/items/makeTEXT/tests/0020-make-text-with-subs/javascript.js
@@ -4,8 +4,12 @@ window.Class6ADEB = class {
   }
 
   signal_6ADEB(ev, el) {
-    const template = "REPLACE_ALFA REPLACE_BRAVO REPLACE_ALFA";
-    const subs = [];
-    el.innerHTML = "xxx";
+    const template = "TARGET_ALFA";
+    const subs = ["TARGET_ALFA", "updated_alfa"];
+    const expected = "updated_alfa";
+    const got = this.api.makeTEXT(template, subs);
+    if (expected === got) {
+      el.innerHTML = "ok";
+    }
   }
 };

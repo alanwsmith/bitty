bfec947ddf60ebb02684cd8f2676339bf8b99597
Alan Smith <commit@alanwsmith.com>
2026-02-13 11:29:48 -0500

    improving docs prose

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/trigger/0015-send-to-method-without-a-receiver/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/trigger/0015-send-to-method-without-a-receiver/javascript.js
@@ -1,17 +1,21 @@
 window.$CLASS_NAME = class {
-  #confirmation = false;
+  #gotSignal = false;
+  #gotNull = false;
 
   bittyReady() {
     this.api.trigger("test_$SIGNAL_NAME");
   }
 
-  test_$SIGNAL_NAME(_, __) {
-    this.#confirmation = true;
+  test_$SIGNAL_NAME(_, x) {
+    this.#gotSignal = true;
+    if (x === null) {
+      this.#gotNull = true;
+    }
     this.api.trigger("verify_$SIGNAL_NAME");
   }
 
   verify_$SIGNAL_NAME(_, el) {
-    if (this.#confirmation === true) {
+    if (this.#gotSignal === true && this.#gotNull === true) {
       el.innerHTML = "ok";
     }
   }
--- content/versions/8/0/0/documentation/includes/0020-api-methods/items/trigger/tests/0015-send-to-method-without-a-receiver/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-api-methods/items/trigger/tests/0015-send-to-method-without-a-receiver/javascript.js
@@ -1,17 +1,21 @@
 window.Class53497 = class {
-  #confirmation = false;
+  #gotSignal = false;
+  #gotNull = false;
 
   bittyReady() {
     this.api.trigger("test_signal_53497");
   }
 
-  test_signal_53497(_, __) {
-    this.#confirmation = true;
+  test_signal_53497(_, x) {
+    this.#gotSignal = true;
+    if (x === null) {
+      this.#gotNull = true;
+    }
     this.api.trigger("verify_signal_53497");
   }
 
   verify_signal_53497(_, el) {
-    if (this.#confirmation === true) {
+    if (this.#gotSignal === true && this.#gotNull === true) {
       el.innerHTML = "ok";
     }
   }

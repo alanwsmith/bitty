41cad64d7f46af0acd4564b436abfee0f275d2f2
Alan Smith <commit@alanwsmith.com>
2026-02-15 18:51:34 -0500

    working on svgs

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/svg/0080-svg-replacement/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/svg/0080-svg-replacement/javascript.js
@@ -1,19 +1,26 @@
 window.$CLASS_NAME = class {
-  #updated;
-
-  bittyReady() {
-    const source = `<svg version="1.1"
+  #sourceSVG = `<svg version="1.1"
      width="300" height="200"
      xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="green" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">TARGET_$HASH</text>
+  TARGET_$HASH
 </svg>`;
-    this.api.addSVG("$ID_NAME", source)
+
+  #secondSVG = `<rect width="100%" height="100%" fill="blue" />
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">ok</text>`;
+
+  #updated;
+
+  bittyReady() {
+    const source = ``;
+    this.api.addSVG("$ID_NAME", this.#sourceSVG)
     this.api.trigger("$SIGNAL_NAME");
   }
 
   $SIGNAL_NAME(_, el) {
-    const subs = { };
+    const subs = {
+      "TARGET_$HASH": this.#secondSVG,
+    };
     this.#updated = this.api.svg("$ID_NAME", subs); 
     el.innerHTML = "xxx";
     this.api.trigger("display_$SIGNAL_NAME");
--- docs/versions/8/0/0/documentation/includes/0020-api-methods/items/svg/tests/0080-svg-replacement/javascript.js
+++ docs/versions/8/0/0/documentation/includes/0020-api-methods/items/svg/tests/0080-svg-replacement/javascript.js
@@ -1,19 +1,26 @@
 window.Class8DE54 = class {
-  #updated;
-
-  bittyReady() {
-    const source = `<svg version="1.1"
+  #sourceSVG = `<svg version="1.1"
      width="300" height="200"
      xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="green" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">TARGET_8DE54</text>
+  TARGET_8DE54
 </svg>`;
-    this.api.addSVG("id-8DE54", source)
+
+  #secondSVG = `<rect width="100%" height="100%" fill="blue" />
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">ok</text>`;
+
+  #updated;
+
+  bittyReady() {
+    const source = ``;
+    this.api.addSVG("id-8DE54", this.#sourceSVG)
     this.api.trigger("signal_8DE54");
   }
 
   signal_8DE54(_, el) {
-    const subs = { };
+    const subs = {
+      "TARGET_8DE54": this.#secondSVG,
+    };
     this.#updated = this.api.svg("id-8DE54", subs); 
     el.innerHTML = "xxx";
     this.api.trigger("display_signal_8DE54");

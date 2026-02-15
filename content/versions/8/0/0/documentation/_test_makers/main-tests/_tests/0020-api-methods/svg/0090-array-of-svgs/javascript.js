a635c5ff92fc4d504903762d60bbce638a49783c
Alan Smith <commit@alanwsmith.com>
2026-02-15 18:58:18 -0500

    finished run with this.api.svg

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/svg/0090-array-of-svgs/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-api-methods/svg/0090-array-of-svgs/javascript.js
@@ -1,21 +1,31 @@
 window.$CLASS_NAME = class {
+  #sourceSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<rect width="100%" height="100%" fill="green" />
+TARGET_$HASH
+</svg>`;
+
+  #secondSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<rect width="100%" height="100%" fill="blue" />
+</svg>`;
+
+  #thirdSVG =  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<text class="$CLASS_NAME" x="150" y="125" font-size="60" text-anchor="middle" fill="goldenrod">ok</text>
+</svg>`;
+
   #updated;
 
   bittyReady() {
-    const source = `<svg version="1.1"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="100%" fill="green" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">TARGET_$HASH</text>
-</svg>`;
-    this.api.addSVG("$ID_NAME", source)
+    const source = ``;
+    this.api.addSVG("$ID_NAME", this.#sourceSVG)
     this.api.trigger("$SIGNAL_NAME");
   }
 
   $SIGNAL_NAME(_, el) {
-    const subs = { };
+    const subs = {
+      "TARGET_$HASH": [this.#secondSVG, this.#thirdSVG]
+    };
     this.#updated = this.api.svg("$ID_NAME", subs); 
-    el.innerHTML = "xxx";
+    el.innerHTML = this.#updated.querySelector(".$CLASS_NAME").innerHTML;
     this.api.trigger("display_$SIGNAL_NAME");
   }
 
--- content/versions/8/0/0/documentation/includes/0020-api-methods/items/svg/tests/0090-array-of-svgs/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-api-methods/items/svg/tests/0090-array-of-svgs/javascript.js
@@ -1,21 +1,31 @@
 window.Class45844 = class {
+  #sourceSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<rect width="100%" height="100%" fill="green" />
+TARGET_45844
+</svg>`;
+
+  #secondSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<rect width="100%" height="100%" fill="blue" />
+</svg>`;
+
+  #thirdSVG =  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<text class="Class45844" x="150" y="125" font-size="60" text-anchor="middle" fill="goldenrod">ok</text>
+</svg>`;
+
   #updated;
 
   bittyReady() {
-    const source = `<svg version="1.1"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="100%" fill="green" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">TARGET_45844</text>
-</svg>`;
-    this.api.addSVG("id-45844", source)
+    const source = ``;
+    this.api.addSVG("id-45844", this.#sourceSVG)
     this.api.trigger("signal_45844");
   }
 
   signal_45844(_, el) {
-    const subs = { };
+    const subs = {
+      "TARGET_45844": [this.#secondSVG, this.#thirdSVG]
+    };
     this.#updated = this.api.svg("id-45844", subs); 
-    el.innerHTML = "xxx";
+    el.innerHTML = this.#updated.querySelector(".Class45844").innerHTML;
     this.api.trigger("display_signal_45844");
   }
 

22987ca7114c5dcaeb72ef48d95bfa6d91423cd9
Alan Smith <commit@alanwsmith.com>
2026-02-17 15:45:54 -0500

    updated setLogLevel so it is a manaul test

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-bit-class-additions/setLogLevel/0020-error-on-invalid/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-bit-class-additions/setLogLevel/0020-error-on-invalid/javascript.js
@@ -1,19 +1,5 @@
 window.$CLASS_NAME = class {
-  bittyReady() {
-    this.trigger("given_$SIGNAL_NAME");
-  }
-
-  given_$SIGNAL_NAME(_, __) {
-    this.trigger("test_$SIGNAL_NAME");
-  }
-
   test_$SIGNAL_NAME(_, el) {
-    this.addLog(
-      "warn",
-      "test_$SIGNAL_NAME",
-      true,
-      "This warning and the upcoming one about setLogLevel being set to an invalid level are expected. They are part of the test suite",
-    );
     this.setLogLevel("not-a-valid-level");
     el.innerHTML = "ok";
   }
--- content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/setLogLevel/tests/0020-error-on-invalid/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/setLogLevel/tests/0020-error-on-invalid/javascript.js
@@ -1,19 +1,5 @@
 window.ClassE2600 = class {
-  bittyReady() {
-    this.trigger("given_signal_E2600");
-  }
-
-  given_signal_E2600(_, __) {
-    this.trigger("test_signal_E2600");
-  }
-
   test_signal_E2600(_, el) {
-    this.addLog(
-      "warn",
-      "test_signal_E2600",
-      true,
-      "This warning and the upcoming one about setLogLevel being set to an invalid level are expected. They are part of the test suite",
-    );
     this.setLogLevel("not-a-valid-level");
     el.innerHTML = "ok";
   }

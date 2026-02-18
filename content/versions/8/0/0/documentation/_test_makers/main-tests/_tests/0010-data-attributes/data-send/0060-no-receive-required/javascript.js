3101037e8d1acea02425d2daf3053d1eda64f570
Alan Smith <commit@alanwsmith.com>
2026-02-17 19:34:45 -0500

    working on data-send docs

---
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0010-data-attributes/data-send/0060-no-receive-required/javascript.js
@@ -0,0 +1,13 @@
+window.$CLASS_NAME = class {
+  bittyReady() {
+    document.querySelector("#$CLICK_CLASS").click();
+  }
+
+  $SIGNAL_NAME(_, __) {
+    this.trigger("verify_$SIGNAL_NAME");
+  }
+
+  verify_$SIGNAL_NAME(_, el) {
+    el.innerHTML = "ok";
+  }
+};
---
+++ docs/versions/8/0/0/documentation/includes/0010-data-attributes/items/data-send/tests/0060-no-receive-required/javascript.js
@@ -0,0 +1,13 @@
+window.Class0773E = class {
+  bittyReady() {
+    document.querySelector("#el-0773E").click();
+  }
+
+  signal_0773E(_, __) {
+    this.trigger("verify_signal_0773E");
+  }
+
+  verify_signal_0773E(_, el) {
+    el.innerHTML = "ok";
+  }
+};

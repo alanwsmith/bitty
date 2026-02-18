a16a2ee7655ae3954825763ad3e53cb5ca2d0720
Alan Smith <commit@alanwsmith.com>
2026-02-17 20:06:28 -0500

    verifying null is the first arg when this.trigger is used

---
+++ content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/trigger/tests/0090-null-argument/javascript.js
@@ -0,0 +1,11 @@
+window.Class88E0C = class {
+  bittyReady() {
+    this.trigger("signal_88E0C");
+  }
+
+  signal_88E0C(ev, el) {
+    if (ev === null) {
+      el.innerHTML = "ok";
+    }
+  }
+};

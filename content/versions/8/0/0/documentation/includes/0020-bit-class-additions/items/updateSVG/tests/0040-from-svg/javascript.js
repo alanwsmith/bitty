7e83a5cf3874b655f0d1b6b25d73fb6ce18c8bdd
Alan Smith <commit@alanwsmith.com>
2026-02-20 10:06:59 -0500

    working on updateSVG

--- content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0040-from-svg/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0040-from-svg/javascript.js
@@ -1,10 +1,10 @@
 window.ClassFE475 = class {
   #key = "svg_signal_FE475";
 
-  test_signal_FE475(input, el) {
-    this.createSVG(this.#key, input);
+  test_signal_FE475(update, el) {
+    this.updateSVG(this.#key, update);
     const svg = this.renderSVG(this.#key);
-    //    el.innerHTML = svg.querySelector("text").innerHTML;
+    el.innerHTML = svg.querySelector("text").innerHTML;
     this.send(svg, "view_signal_FE475");
   }
 
@@ -17,11 +17,21 @@ window.ClassFE475 = class {
   /////////////////////////////////////////////////
 
   bittyReady() {
-    const input = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    input.setAttribute("width", "60");
-    input.setAttribute("height", "40");
-    input.innerHTML = `<rect width="100%" height="100%" fill="green" />
-  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">ok</text>`;
-    this.send(input, "test_signal_FE475");
+    this.setLogLevel("none");
+    this.deleteSVG(this.#key);
+    const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">bug</text>
+</svg>`;
+    this.createSVG(this.#key, input);
+    const template = document.createElement("template");
+    template.innerHTML = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">ok</text>
+</svg>`;
+    const update = template.content.querySelector("svg");
+    this.send(update, "test_signal_FE475");
   }
 };

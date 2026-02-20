2fa89d6d539900b538076c68c0b6d2c4664d93fe
Alan Smith <commit@alanwsmith.com>
2026-02-20 10:14:37 -0500

    working on updateSVG

--- content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-bit-class-additions/updateSVG/0070-do-not-warn-on-overwrite/javascript.js
+++ content/versions/8/0/0/documentation/_test_makers/main-tests/_tests/0020-bit-class-additions/updateSVG/0070-do-not-warn-on-overwrite/javascript.js
@@ -1,11 +1,16 @@
 window.$CLASS_NAME = class {
   #key = "svg_$SIGNAL_NAME";
 
-  test_$SIGNAL_NAME(input, el) {
-    const result = this.createSVG(this.#key, input);
-    if (result.ok === true && result.level === "warn") {
-      //      el.innerHTML = "ok";
+  test_$SIGNAL_NAME(update, el) {
+    const result = this.updateSVG(this.#key, update);
+    if (result.ok === true && result.level === "info") {
+      el.innerHTML = "ok";
     }
+    this.trigger("view_$SIGNAL_NAME");
+  }
+
+  view_$SIGNAL_NAME(svg, el) {
+    el.replaceWith(this.renderSVG(this.#key));
   }
 
   /////////////////////////////////////////////////
@@ -13,13 +18,19 @@ window.$CLASS_NAME = class {
   /////////////////////////////////////////////////
 
   bittyReady() {
+    this.setLogLevel("none");
+    this.deleteSVG(this.#key);
     const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">bug</text>
+</svg>`;
+    this.createSVG(this.#key, input);
+    const update = `
 <svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="green" />
   <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">ok</text>
 </svg>`;
-    this.setLogLevel("none");
-    this.createSVG(this.#key, input);
-    this.send(input, "test_$SIGNAL_NAME");
+    this.send(update, "test_$SIGNAL_NAME");
   }
 };
--- docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0060-store-automatically/javascript.js
+++ docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0060-store-automatically/javascript.js
@@ -4,7 +4,7 @@ window.Class2EB3B = class {
   test_signal_2EB3B(_, el) {
     this.loadSVG(this.#key);
     const svg = this.renderSVG(this.#key);
-    //    el.innerHTML = svg.querySelector("text").innerHTML;
+    el.innerHTML = svg.querySelector("text").textContent;
     this.send(svg, "view_signal_2EB3B");
   }
 
@@ -17,15 +17,20 @@ window.Class2EB3B = class {
   /////////////////////////////////////////////////
 
   bittyReady() {
+    this.setLogLevel("none");
+    this.deleteSVG(this.#key);
     const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">bug</text>
+</svg>`;
+    this.createSVG(this.#key, input);
+    const update = `
 <svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="green" />
   <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">ok</text>
 </svg>`;
-    this.setLogLevel("none");
-    this.removeSVG(this.#key);
-    this.createSVG(this.#key, input);
-    delete this._svg[this.#key];
+    this.updateSVG(this.#key, update);
     this.trigger("test_signal_2EB3B");
   }
 };

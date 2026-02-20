78f38b5d0cc1ac8cd71f06db320b0e6145b2b3bf
Alan Smith <commit@alanwsmith.com>
2026-02-20 00:38:45 -0500

    working on createSVG

--- docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/createSVG/tests/0070-warn-on-overwrite/javascript.js
+++ docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/createSVG/tests/0070-warn-on-overwrite/javascript.js
@@ -1,10 +1,10 @@
 window.ClassFACB7 = class {
   #key = "fragment_signal_FACB7";
 
-  test_signal_FACB7(_, el) {
-    const result = this.createFragment(this.#key, `<div></div>`);
+  test_signal_FACB7(input, el) {
+    const result = this.createFragment(this.#key, input);
     if (result.ok === true && result.level === "warn") {
-      //      el.innerHTML = "ok";
+      el.innerHTML = "ok";
     }
   }
 
@@ -13,12 +13,13 @@ window.ClassFACB7 = class {
   /////////////////////////////////////////////////
 
   bittyReady() {
-    this.trigger("given_signal_FACB7");
-  }
-
-  given_signal_FACB7(_, __) {
+    const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">ok</text>
+</svg>`;
     this.setLogLevel("none");
-    this.createFragment(this.#key, `<div></div>`);
-    this.trigger("test_signal_FACB7");
+    this.createFragment(this.#key, input);
+    this.send(input, "test_signal_FACB7");
   }
 };

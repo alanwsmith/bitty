290a28a8c48b960160f750f409d81a099354c956
Alan Smith <commit@alanwsmith.com>
2026-02-20 10:23:22 -0500

    finished run with updateSVG

--- content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0090-no-key-no-value-returns-error/javascript.js
+++ content/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0090-no-key-no-value-returns-error/javascript.js
@@ -2,9 +2,9 @@ window.ClassDECD9 = class {
   #key = "svg_signal_DECD9";
 
   test_signal_DECD9(_, el) {
-    const result = this.createSVG();
+    const result = this.updateSVG();
     if (result.ok === false && result.level === "error") {
-      //      el.innerHTML = "ok";
+      el.innerHTML = "ok";
     }
   }
 
@@ -14,6 +14,13 @@ window.ClassDECD9 = class {
 
   bittyReady() {
     this.setLogLevel("none");
+    this.deleteSVG(this.#key);
+    const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">bug</text>
+</svg>`;
+    this.createSVG(this.#key, input);
     this.trigger("test_signal_DECD9");
   }
 };
--- docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0077-ok-false-on-failure/javascript.js
+++ docs/versions/8/0/0/documentation/includes/0020-bit-class-additions/items/updateSVG/tests/0077-ok-false-on-failure/javascript.js
@@ -1,12 +1,12 @@
 window.Class00957 = class {
   #key = "svg_signal_00957";
 
-  test_signal_00957(input, el) {
-    const result = this.createSVG(this.#key, {
-      key: "not a string or svg",
+  test_signal_00957(_, el) {
+    const result = this.updateSVG(this.#key, {
+      misc: "not a valid input for an svg",
     });
     if (result.ok === false && result.level === "error") {
-      //      el.innerHTML = "ok";
+      el.innerHTML = "ok";
     }
   }
 
@@ -16,6 +16,13 @@ window.Class00957 = class {
 
   bittyReady() {
     this.setLogLevel("none");
+    this.deleteSVG(this.#key);
+    const input = `
+<svg version="1.1" width="60" height="40" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="green" />
+  <text x="30" y="24" font-size="20" text-anchor="middle" fill="white">bug</text>
+</svg>`;
+    this.createSVG(this.#key, input);
     this.trigger("test_signal_00957");
   }
 };

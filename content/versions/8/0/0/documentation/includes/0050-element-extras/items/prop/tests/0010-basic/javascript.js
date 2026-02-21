806781061ec675ad386b55999ed3369e03b7dc04
Alan Smith <commit@alanwsmith.com>
2026-02-20 19:33:37 -0500

    working on stubbing element extras

---
+++ content/versions/8/0/0/documentation/includes/0050-element-extras/items/prop/tests/0010-basic/javascript.js
@@ -0,0 +1,13 @@
+window.Class3444A = class {
+  test_signal_3444A(ev, el) {
+    el.innerHTML = ev.prop("needle");
+  }
+
+  /////////////////////////////////////////////////
+  // Test Setup
+  /////////////////////////////////////////////////
+
+  bittyReady() {
+    document.querySelector("#el-3444A").click();
+  }
+};

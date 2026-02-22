6365f9096c6e3aa6a4f38e3ca6baf75f023e1a48
Alan Smith <commit@alanwsmith.com>
2026-02-22 00:52:35 -0500

    getting ready to move everything but the details out

--- docs/versions/8/0/0/documentation/includes/0050-element-extras/items/getValueAsInt/tests/0010-basic/javascript.js
+++ docs/versions/8/0/0/documentation/includes/0050-element-extras/items/getValueAsInt/tests/0010-basic/javascript.js
@@ -1,5 +1,12 @@
 window.Class05EDE = class {
-  test_signal_05EDE(_, el) {
+  bittyReady() {
+    document
+      .querySelector("[data-receive=signal_05EDE]")
+      .value = 9000;
+    this.trigger("signal_05EDE");
+  }
+
+  signal_05EDE(_, el) {
     if (el.getValueAsInt() === 9000) {
       this.trigger("verify_signal_05EDE");
     }
@@ -7,13 +14,5 @@ window.Class05EDE = class {
 
   verify_signal_05EDE(_, el) {
     el.innerHTML = "ok";
-  }
-
-  /////////////////////////////////////////////////
-  // Test Setup
-  /////////////////////////////////////////////////
-
-  bittyReady() {
-    this.trigger("test_signal_05EDE");
   }
 };

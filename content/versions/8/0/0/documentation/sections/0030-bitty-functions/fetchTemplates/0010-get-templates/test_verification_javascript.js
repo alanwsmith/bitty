3eaf6f9620d6f0ffd29e860e7de050859d8fb46b
Alan Smith <commit@alanwsmith.com>
2026-02-28 12:44:15 -0500

    working on test layou

--- content/versions/8/0/0/documentation/sections/0030-bitty-functions/fetchTemplates/0010-get-templates/test_verification_javascript.js
+++ content/versions/8/0/0/documentation/sections/0030-bitty-functions/fetchTemplates/0010-get-templates/test_verification_javascript.js
@@ -18,22 +18,3 @@ export async function runTest() {
     }
   });
 }
-
-/*
-export async function verify_signal_8958C(_, __, ___) {
-  const pattern = /\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/;
-  const checkEls = bitty.qsa("[data-r~=signal_8958C]");
-  checkEls.forEach((checkEl) => {
-    if (checkEl.innerHTML === "todo") {
-      checkEl.dataset.testStatus = 1;
-    } else {
-      const match = checkEl.innerHTML.match(pattern);
-      if (match !== null) {
-        checkEl.dataset.testStatus = 0;
-      } else {
-        checkEl.dataset.testStatus = 2;
-      }
-    }
-  });
-}
-*/

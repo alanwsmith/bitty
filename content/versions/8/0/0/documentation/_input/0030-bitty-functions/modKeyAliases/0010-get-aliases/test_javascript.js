d5f9a2d0537fe4f23a59978206d0d9504955da05
Alan Smith <commit@alanwsmith.com>
2026-03-07 14:11:16 -0500

    added test for modKeyAliases

---
+++ content/versions/8/0/0/documentation/_input/0030-bitty-functions/modKeyAliases/0010-get-aliases/test_javascript.js
@@ -0,0 +1,7 @@
+export const bitty = {};
+
+export function $_SIGNAL_(_, __, el) {
+  if (bitty.modKeyAliases().alt === "altKey") {
+    el.innerHTML = bitty.localTimestamp();
+  }
+}
---
+++ content/versions/8/0/0/documentation/sections/0030-bitty-functions/modKeyAliases/0010-get-aliases/test_javascript.js
@@ -0,0 +1,7 @@
+export const bitty = {};
+
+export function signal_13C5D(_, __, el) {
+  if (bitty.modKeyAliases().alt === "altKey") {
+    el.innerHTML = bitty.localTimestamp();
+  }
+}

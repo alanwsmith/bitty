8fa0ee8c04f1de37568c6611239c0af5ac246ba6
Alan Smith <commit@alanwsmith.com>
2026-03-07 15:13:31 -0500

    finished qs stuff

---
+++ content/versions/8/0/0/documentation/_input/0030-bitty-functions/qs/0020-from-element/test_javascript.js
@@ -0,0 +1,10 @@
+export const bitty = {};
+
+export function $_SIGNAL_(ev, sender, el) {
+  const fromNative = document.querySelector("#target_bravo_$_SIGNAL_");
+  const alfa = bitty.qs("#target_alfa_$_SIGNAL_");
+  const bravo = bitty.qs("#target_bravo_$_SIGNAL_", alfa);
+  if (bravo.isSameNode(fromNative)) {
+    el.innerHTML = bitty.localTimestamp();
+  }
+}
--- content/versions/8/0/0/documentation/sections/0030-bitty-functions/qs/0020-from-element/test_javascript.js
+++ content/versions/8/0/0/documentation/sections/0030-bitty-functions/qs/0020-from-element/test_javascript.js
@@ -1,7 +1,10 @@
 export const bitty = {};
 
 export function signal_A9BED(ev, sender, el) {
-  // el.dataset.solo = true;
-  el.innerHTML = "todo";
-  // el.innerHTML = bitty.localTimestamp();
+  const fromNative = document.querySelector("#target_bravo_signal_A9BED");
+  const alfa = bitty.qs("#target_alfa_signal_A9BED");
+  const bravo = bitty.qs("#target_bravo_signal_A9BED", alfa);
+  if (bravo.isSameNode(fromNative)) {
+    el.innerHTML = bitty.localTimestamp();
+  }
 }

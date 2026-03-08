56c30037cf84f206a2650cd962732ae92a17053c
Alan Smith <commit@alanwsmith.com>
2026-03-07 22:53:10 -0500

    working on bitty send

--- content/versions/8/0/0/documentation/_input/0030-bitty-functions/send/0020-multiple-sends/test_verification_javascript.js
+++ content/versions/8/0/0/documentation/_input/0030-bitty-functions/send/0020-multiple-sends/test_verification_javascript.js
@@ -4,8 +4,21 @@ export async function runTest() {
   bitty.qs("[data-s~=$_SIGNAL_]").click();
   await bitty.sleep(200);
   const pattern = /\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/;
-  const checkEls = bitty.qsa("[data-r~=$_SIGNAL_]");
+  const checkEls = bitty.qsa("[data-r~=$_SIGNAL2_]");
   checkEls.forEach((checkEl) => {
+    if (checkEl.innerHTML === "todo") {
+      checkEl.dataset.testStatus = 1;
+    } else {
+      const match = checkEl.innerHTML.match(pattern);
+      if (match !== null) {
+        checkEl.dataset.testStatus = 0;
+      } else {
+        checkEl.dataset.testStatus = 2;
+      }
+    }
+  });
+  const checkEls3 = bitty.qsa("[data-r~=$_SIGNAL3_]");
+  checkEls3.forEach((checkEl) => {
     if (checkEl.innerHTML === "todo") {
       checkEl.dataset.testStatus = 1;
     } else {

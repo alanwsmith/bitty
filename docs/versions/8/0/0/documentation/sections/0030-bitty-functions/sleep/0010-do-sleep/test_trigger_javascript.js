6f9334f2821d3d1f0aaa0b2b3a304ba5d24467e8
Alan Smith <commit@alanwsmith.com>
2026-03-07 21:04:10 -0500

    added test for sleep

---
+++ docs/versions/8/0/0/documentation/sections/0030-bitty-functions/sleep/0010-do-sleep/test_trigger_javascript.js
@@ -0,0 +1,12 @@
+//  DEPRECATED: Remove these _test_trigger_javascript.js files
+//
+//export const bitty = {};
+/*
+
+export async function run_signal_B5E30() {
+  await bitty.sleep(200);
+  bitty.qs("[data-s~=signal_B5E30]").click();
+  await bitty.sleep(300);
+  bitty.trigger("verify_signal_B5E30");
+}
+*/

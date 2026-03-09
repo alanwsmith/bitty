0859eb6478d7e777f10ac3ddc22240356310f01e
Alan Smith <commit@alanwsmith.com>
2026-03-09 13:52:00 -0400

    added test for setProp

--- content/versions/8/0/0/documentation/_input/0060-element-extras/setProp/0010-set-prop/test_javascript.js
+++ content/versions/8/0/0/documentation/_input/0060-element-extras/setProp/0010-set-prop/test_javascript.js
@@ -1,8 +1,8 @@
 export const bitty = {};
 
 export function $_SIGNAL_(_, __, el) {
-  sender.setProp("update", "$_TEST_ID_");
-  if (sender.dataset.update === "$_TEST_ID_") {
+  el.setProp("update", "$_TEST_ID_");
+  if (el.dataset.update === "$_TEST_ID_") {
     el.innerHTML = bitty.time();
   }
 }
--- content/versions/8/0/0/documentation/sections/0060-element-extras/setProp/0010-set-prop/test_javascript.js
+++ content/versions/8/0/0/documentation/sections/0060-element-extras/setProp/0010-set-prop/test_javascript.js
@@ -1,8 +1,8 @@
 export const bitty = {};
 
 export function signal_A090D_alfa(_, __, el) {
-  sender.setProp("update", "A090D_alfa");
-  if (sender.dataset.update === "A090D_alfa") {
+  el.setProp("update", "A090D_alfa");
+  if (el.dataset.update === "A090D_alfa") {
     el.innerHTML = bitty.time();
   }
 }

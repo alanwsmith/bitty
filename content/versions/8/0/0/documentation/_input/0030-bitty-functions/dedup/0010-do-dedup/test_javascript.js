3c8dc5a2dee29c401018a34cacc67f7d11e2673e
Alan Smith <commit@alanwsmith.com>
2026-03-12 13:37:38 -0400

    moved tests for render and svg to next iteration

---
+++ content/versions/8/0/0/documentation/_input/0030-bitty-functions/dedup/0010-do-dedup/test_javascript.js
@@ -0,0 +1,9 @@
+export const b = {};
+
+export function $_SIGNAL_(ev, sender, el) {
+  const input_array = ["d", "a", "b", "b", "a", "c", "d"];
+  const check_array = b.dedup(input_array);
+  if (JSON.stringify(check_array) === JSON.stringify(["d", "a", "b", "c"])) {
+    el.innerHTML = b.time();
+  }
+}

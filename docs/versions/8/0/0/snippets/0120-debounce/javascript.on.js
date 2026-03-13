7d9ac7e886f559d502a3e5a2f1cc2ed8a5722eb0
Alan Smith <commit@alanwsmith.com>
2026-03-13 00:52:58 -0400

    working on snippets

---
+++ docs/versions/8/0/0/snippets/0120-debounce/javascript.on.js
@@ -0,0 +1,10 @@
+export const b = {};
+
+export function start(ev, __, el) {
+  ev.target.innerHTML = "waiting for debounce to resolve";
+  b.debounce("snippet", "end", 2000);
+}
+
+export function end(_, __, el) {
+  el.innerHTML = "debounce has resolved";
+}

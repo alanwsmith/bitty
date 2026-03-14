846d2258c59bcca84aaaccdd8f63dc51474e06fe
Alan Smith <commit@alanwsmith.com>
2026-03-13 21:39:20 -0400

    moved snippets to alphabetized

---
+++ docs/versions/8/0/0/snippets/get-remote-data/javascript.on.js
@@ -0,0 +1,12 @@
+export const b = { init: "init" };
+
+const url = `/versions/8/0/0/snippets/get-remote-data/data.json`;
+
+export async function init(_, __, el) {
+  const data = await b.get(url);
+  if (data) {
+    el.innerHTML = data.status;
+  } else {
+    el.innerHTML = "could not load data";
+  }
+}

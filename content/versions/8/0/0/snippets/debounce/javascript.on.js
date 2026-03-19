6e1d64d3e7eb27124e0f43b5b54ad3ef57a6f85d
Alan Smith <commit@alanwsmith.com>
2026-03-19 19:46:52 -0400

    working on overviews for snippets

--- content/versions/8/0/0/snippets/debounce/javascript.on.js
+++ content/versions/8/0/0/snippets/debounce/javascript.on.js
@@ -1,10 +1,13 @@
 export const b = {};
 
+let count = 0;
+
 export function start(ev, __, el) {
   ev.target.innerHTML = "waiting for debounce to resolve";
   b.debounce("snippet", "end", 2000);
 }
 
 export function end(_, __, el) {
-  el.innerHTML = "debounce has resolved";
+  count += 1;
+  el.innerHTML = `debounce has resolved to count ${count}`;
 }
